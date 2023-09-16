e53b0ec1e7e07cbae786e13e43675cc442e62020
unknown <cpt.gege@gmail.com>
2023-09-17 09:31:17 +1000

    test sql

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
-const express = require("express");
+import express from "express";
+import mysql from "mysql";
+
+const connection = mysql.createConnection({
+  host: "db-instance.crruajoumg3g.ap-southeast-2.rds.amazonaws.com",
+  port: 3306,
+  user: "admin",
+  password: "Daovuhoangnam99",
+  database: "bright_boost_schema",
+});
+
+connection.connect();
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from Bright Boost!" });
+  connection.query("select * from subject", (err, rows) => {
+    if (err) throw err;
+
+    console.log(rows[0]);
+    res.json({ rows });
+  });
 });
 
 app.listen(PORT, () => {
