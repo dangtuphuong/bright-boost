83903fc77044500c24d93e3c9fa0f2fa53c54fb3
Phuong Dang <tuphuong.dang@gmail.com>
2023-09-18 18:55:44 +1000

    add login page

--- client/src/routes/login-page.jsx
+++ client/src/routes/login-page.jsx
@@ -1,40 +1,63 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "./login-page.scss";
 
-export default function LoginPage() {
-  const [email, setEmail] = useState();
-  const [pass, setPass] = useState();
+import AuthService from "../services/auth-service";
 
-  const onSubmit = () => console.log(email, pass);
+export default function LoginPage() {
+  let navigate = useNavigate();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    console.log(email, password);
+
+    AuthService.login(email, password).then(
+      () => {
+        navigate("/home");
+        window.location.reload();
+      },
+      (error) => {
+        console.log(error);
+        setLoading(false);
+      }
+    );
+  };
 
   return (
     <div className="container">
       <div className="row">
         <div className="col loginImg"></div>
-        <div className="col d-flex justify-content-center align-items-center loginForm">
-          <Form className="formWrap">
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                onChange={(e) => setPass(e.target.value)}
-              />
-            </Form.Group>
-            <Button variant="primary" onClick={onSubmit}>
-              Sign In
-            </Button>
-          </Form>
+        <div className="col d-flex flex-column justify-content-center align-items-center loginForm">
+          <div>
+            <h3>Welcome Back!</h3>
+            <Form className="formWrap">
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
+              <Button variant="primary" onClick={onSubmit}>
+                Sign In
+              </Button>
+            </Form>
+          </div>
         </div>
       </div>
     </div>
