292e45c430a5361611f20f40cad2501a43bbef95
Phuong Dang <tuphuong.dang@gmail.com>
2023-09-24 17:14:05 +1000

    add toast message component

---
+++ client/src/components/Toast.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Toast } from "react-bootstrap";
+
+const ToastMessage = ({ text, textHeader = null, show, onHide, type }) => {
+  return (
+    <Toast
+      show={show}
+      onClose={onHide}
+      bg={type}
+      style={{ position: "fixed", top: "20px", right: "20px" }}
+    >
+      <Toast.Header>
+        <strong className="w-100">{textHeader || "Notification"}</strong>
+      </Toast.Header>
+      {<Toast.Body>{text}</Toast.Body>}
+    </Toast>
+  );
+};
+
+const createToast =
+  (type) =>
+  (text, textHeader = null, duration = 7000) => {
+    const toastContainer = document.createElement("div");
+    document.body.appendChild(toastContainer);
+    const toastRoot = createRoot(toastContainer);
+
+    return new Promise((resolve) => {
+      const hideToast = () => {
+        toastRoot.unmount();
+        toastContainer.remove();
+        resolve();
+      };
+
+      const showToast = () => {
+        toastRoot.render(
+          <ToastMessage
+            text={text}
+            textHeader={textHeader}
+            show={true}
+            onHide={hideToast}
+            type={
+              (type === "error" && "danger") ||
+              (type === "success" && "success")
+            }
+          />
+        );
+        setTimeout(() => {
+          hideToast();
+        }, duration);
+      };
+
+      showToast();
+    });
+  };
+
+export const toast = {
+  error: createToast("error"),
+  success: createToast("success"),
+};
+
+export default ToastMessage;
