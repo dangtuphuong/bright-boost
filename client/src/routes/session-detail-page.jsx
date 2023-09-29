6106117735538dfa51198e5192097984338fdbda
Phuong Dang <tuphuong.dang@gmail.com>
2023-09-29 15:11:29 +1000

    add session detail page

--- client/src/routes/session-detail-page.jsx
+++ client/src/routes/session-detail-page.jsx
@@ -1,16 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import Spinner from "react-bootstrap/Spinner";
+import classNames from "classnames";
+
+import "./session-detail-page.scss";
 
 import NavBar from "../components/NavBar";
+import DataService from "../services/data-service";
+import { toast } from "../components/Toast";
 
 const SessionDetailPage = () => {
   const { id } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [students, setStudents] = useState([]);
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    DataService.getSessionDetail()
+      .then((data) => {
+        setQuestions(data?.questions || []);
+        setStudents(data?.students || []);
+      })
+      .catch((err) => toast.error(err?.message))
+      .finally(() => setLoading(false));
+  }, []);
+
+  console.log(questions);
 
   return (
     <NavBar>
       <div className="container">
-        <h1>SessionDetailPage</h1>
-        <p>Session ID: {id}</p>
+        <h2 className="mb-4 mt-4">Session ID: {id}</h2>
+        <div className="row">
+          <div className="col">
+            <h4 className="mb-4">Student List</h4>
+            {loading ? (
+              <Spinner animation="border" />
+            ) : (
+              students?.map((student) => (
+                <Card className="mb-3" key={student?.id}>
+                  <Card.Body>
+                    <div>{student?.name}</div>
+                    <div className="color-light">{student?.email}</div>
+                  </Card.Body>
+                </Card>
+              ))
+            )}
+          </div>
+          <div className="col">
+            <h4 className="mb-4">Question List</h4>
+            {loading ? (
+              <Spinner animation="border" />
+            ) : (
+              questions?.map((question) => (
+                <Card
+                  key={question?.id}
+                  className={classNames("mb-3", {
+                    isAnswered: question?.isAnswered,
+                  })}
+                >
+                  <Card.Body>
+                    <div>{question?.content}</div>
+                    <div>
+                      <div className="color-light">{`From ${question?.studentName}`}</div>
+                      {question?.tutorName && (
+                        <div className="color-light">{`Answered by ${question?.tutorName}`}</div>
+                      )}
+                    </div>
+                    {!!question?.isAnswered && (
+                      <i className="material-icons check-icon">check_circle</i>
+                    )}
+                  </Card.Body>
+                </Card>
+              ))
+            )}
+          </div>
+        </div>
       </div>
     </NavBar>
   );
