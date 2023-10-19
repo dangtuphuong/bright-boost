f4f57cbbdaee053a5f07da3b88b840c03bb80d6f
Phuong Dang <tuphuong.dang@gmail.com>
2023-10-19 23:54:57 +1100

    reduce toast time

--- client/src/components/Toast.jsx
+++ client/src/components/Toast.jsx
@@ -20,7 +20,7 @@ const ToastMessage = ({ text, textHeader = null, show, onHide, type }) => {
 
 const createToast =
   (type) =>
-  (text, textHeader = null, duration = 3000) => {
+  (text, textHeader = null, duration = 1500) => {
     const toastContainer = document.createElement("div");
     document.body.appendChild(toastContainer);
     const toastRoot = createRoot(toastContainer);
