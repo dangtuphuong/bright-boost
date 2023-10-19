a4dbf8ff03f605dc2261d7850e4ff5460f4be80c
Phuong Dang <tuphuong.dang@gmail.com>
2023-10-20 00:34:19 +1100

    add subject to form

--- client/src/components/QuestionInput.jsx
+++ client/src/components/QuestionInput.jsx
@@ -2,6 +2,8 @@ import React, { useState, useMemo } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Spinner from "react-bootstrap/Spinner";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 import DataService from "../services/data-service";
 import AuthService from "../services/auth-service";
@@ -9,12 +11,23 @@ import { toast } from "../components/Toast";
 
 import "./QuestionInput.scss";
 
+const subjects = [
+  { id: 1, name: "Maths" },
+  { id: 2, name: "Physics" },
+  { id: 3, name: "Literature" },
+  { id: 4, name: "Chemistry" },
+  { id: 5, name: "Geography" },
+  { id: 6, name: "Arts" },
+  { id: 7, name: "Biology" },
+];
+
 const QuestionInput = ({ sessionId, onSubmitSuccess }) => {
   const [studentId, setStudentId] = useState("");
+  const [subjectId, setSubjectId] = useState("");
   const [content, setContent] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const disabled = !content?.length || !studentId;
+  const disabled = !content?.length || !studentId || !subjectId;
 
   const currentUser = useMemo(() => AuthService.getCurrentUser(), []);
   const isStudent = currentUser?.role === "student";
@@ -31,11 +44,13 @@ const QuestionInput = ({ sessionId, onSubmitSuccess }) => {
     DataService.postQuestion({
       sessionId,
       studentId: Number(studentId),
+      subjectId,
       content,
     })
       .then(() => {
         setLoading(false);
         setStudentId("");
+        setSubjectId("");
         setContent("");
         onSubmitSuccess();
       })
@@ -47,17 +62,38 @@ const QuestionInput = ({ sessionId, onSubmitSuccess }) => {
 
   return (
     <Form className="w-100 question-input-wrapper">
-      {!isStudent && (
-        <Form.Group className="mb-3">
-          <Form.Label>Student ID</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Student ID"
-            value={studentId}
-            onChange={(e) => setStudentId(e?.target?.value)}
-          />
-        </Form.Group>
-      )}
+      <Row>
+        {!isStudent && (
+          <Col>
+            <Form.Group className="mb-3">
+              <Form.Label>Student ID</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Student ID"
+                value={studentId}
+                onChange={(e) => setStudentId(e?.target?.value)}
+              />
+            </Form.Group>
+          </Col>
+        )}
+        <Col>
+          <Form.Group className="mb-3">
+            <Form.Label>Subject</Form.Label>
+            <Form.Select
+              value={subjectId}
+              onChange={(e) => setSubjectId(e?.target?.value)}
+            >
+              <option>Choose one subject</option>
+              {subjects?.map((s) => (
+                <option key={s?.id} value={s?.id}>
+                  {s?.name}
+                </option>
+              ))}
+            </Form.Select>
+          </Form.Group>
+        </Col>
+      </Row>
+
       <Form.Group className="mb-3">
         <Form.Label>Question</Form.Label>
         <Form.Control
