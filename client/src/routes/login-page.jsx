5e4aa18123404a1489a7c7ac27014e632d422455
Adnan <adnan.techme@gmail.com>
2023-10-20 14:49:42 +1100

    fixed reference link for image on login page

--- client/src/routes/login-page.jsx
+++ client/src/routes/login-page.jsx
@@ -48,7 +48,7 @@ export default function LoginPage() {
     <div className="container">
       <div className="row">
         <div className="col d-flex flex-column justify-content-center align-items-center">
-        <a href="https://storyset.com/illustration/college-class/rafiki">
+        <a href="https://storyset.com/illustration/college-class/rafiki" target="_blank">
           <img className="heroImg" src={heroImg}/>
         </a>
         </div>
